39f7fa1aeb230eba0d643fe51f17b65d91f28ed5
adhamaly <adhamaly126@gmail.com>
2025-01-03 21:51:11 +0200

    feat: added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Rabbit Mart System APIs')
+    .setDescription('API documentation for the Rabbit Mart System APIs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('rabbit-mart/docs', app, document);
 
   await app.listen(8080);
 }
